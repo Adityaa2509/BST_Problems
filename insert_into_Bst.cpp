dbef002cc1caecfb34f8659b54d1d339a338befa
Aditya Kuntal <geekysingh2502@gmail.com>
2023-05-29 17:34:00 +0530

    Leetcode - 701

---
+++ insert_into_Bst.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+};
+void insert2bst(node*& root,int data)
+{
+    if(root == NULL)
+    {
+        root = new node(data);
+        return ;
+    }
+if(root->data<data)insert2bst(root->right,data);
+else insert2bst(root->left,data);
+}
+
+void takeInput(node*& root)
+{
+    cout<<"Enter data for BST"<<endl;
+    int data;
+    cin>>data;
+    while(data!=-1)
+    {
+        insert2bst(root,data);
+        cin>>data;
+    }
+}
+void LOD(node* root)
+{
+    if(root == NULL)return ;
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty())
+    {
+        node* temp = q.front();
+        q.pop();
+        if(temp == NULL)
+        {
+            cout<<endl;
+            if(!q.empty())q.push(NULL);
+        }
+        else
+        {
+            cout<<temp->data<<" ";
+            if(temp->left)q.push(temp->left);
+            if(temp->right)q.push(temp->right);
+        }
+    }
+    cout<<endl;
+}
+int main()
+{
+    node* root = NULL;
+    takeInput(root);
+    LOD(root);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
